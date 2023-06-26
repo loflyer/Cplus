7b2614fc6d835df86837b50c04ef4183613c3052
‘loflyer’ <klxxf4@gmail.com>
2023-06-26 10:21:11 +0800

    modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -11,6 +11,8 @@ int main(int argc, char const *argv[])
     swt.show_stn();
 
     // 如果重载赋值运算符而不对该函数作任何处理， 下面代码的赋值（隐式类型转换就转不了） 
+
+    // 对的，没有错！！
     swt = 250;
     swt.show_stn();
     return 0;
